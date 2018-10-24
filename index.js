29fbe7a079c091d48a20025c2be8e04dca756803
hicham maghzaz <h.maghzaz@yahoo.fr>
2018-10-24 06:09:12 +0100

    switch between to image

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 var button = document.getElementById("submitButton");
 var images = document.getElementsByTagName("img");
-var image = images[0];
+var image1 = images[0];
+var image2 = images[1];
 
-button.onclick = function(event) {
-  image.hidden = !image.hidden;
-};
+image1.hidden = false;
+image2.hidden = true;
+
+image1.onclick = function(event)
+{
+   image1.hidden = true;
+   image2.hidden = false;
+}
+
+image2.onclick = function(event) 
+{
+    image1.hidden = false;
+    image2.hidden = true;
+}
+
+
